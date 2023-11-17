b436f470448f53b199f6b0659e509e75bcaebb80
alamnahid <nahidalam13765@gmail.com>
2023-11-17 11:32:08 +0600

    create api for add foods

--- index.js
+++ index.js
@@ -70,11 +70,17 @@ async function run() {
         return res.status(403).send({ message: 'forbidden access' });
       }
       next();
-    }
+    }  
 
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/menu',verifyToken, verifyAdmin, async(req, res)=>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
       res.send(result)
     })
 
