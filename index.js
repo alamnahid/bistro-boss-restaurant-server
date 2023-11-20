390b249f6ceabe21e4622b06700ad9d48ada8def
alamnahid <nahidalam13765@gmail.com>
2023-11-20 18:16:24 +0600

    implement stripe payment gateway

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express();
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+      'http://localhost:5173',
+      // 'https://onebistroboss.web.app',
+      // 'https://onebistroboss.firebaseapp.com'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -35,6 +43,8 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews")
     const cartCollection = client.db("bistroDb").collection("carts")
     const usersCollection = client.db("bistroDb").collection("users")
+    const contactCollection = client.db("bistroDb").collection("contact")
+    const paymentCollection = client.db("bistroDb").collection("payments")
 
 
     // jwt related apis
@@ -141,6 +151,51 @@ async function run() {
       res.send(result);
     })
 
+    //payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
+
+    // payment related apis
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //  carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    })
+
+    app.get('/payments/:email', verifyToken, async (req, res) => {
+      const query = { email: req.params.email }
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
 
     // users related apis 
     app.post('/users', async (req, res) => {
@@ -194,6 +249,19 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updatedDoc);
       res.send(result)
 
+    })
+
+
+    // conatct message apis
+    app.post('/contact', async(req, res)=>{
+      const item = req.body;
+      const result = await contactCollection.insertOne(item);
+      res.send(result)
+    })
+
+    app.get('/contact', async (req, res) => {
+      const result = await contactCollection.find().toArray()
+      res.send(result)
     })
 
 
